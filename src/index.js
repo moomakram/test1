b5a7cf9cfdffcd84438da6d57e0da1a15a65a8a8
Your Name <you@example.com>
2025-04-21 20:05:26 +0200

    تعديلات لتحسين العرض على الموبايل

--- src/index.js
+++ src/index.js
@@ -6,13 +6,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';  // تعديل هنا: استبدال BrowserRouter بـ HashRouter
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
+  <HashRouter>  {/* تعديل هنا: استخدام HashRouter */}
     <App />
-  </BrowserRouter>
+  </HashRouter>
 );
 
 reportWebVitals();
